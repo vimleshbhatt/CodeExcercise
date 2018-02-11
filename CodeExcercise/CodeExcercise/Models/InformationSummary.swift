708a17a91f35c089b760abd84bd6146dbb200d92
Vimlesh Bhatt <vimlesh.bhatt@fgb.ae>
2018-02-12 00:09:35 +0400

    Implemented a basic model object to  hold the information summary

---
+++ CodeExcercise/CodeExcercise/Models/InformationSummary.swift
@@ -0,0 +1,23 @@
+//
+//  InformationSummary.swift
+//  CodeExcercise
+//
+//  Created by fgb on 2/12/18.
+//  Copyright © 2018 vimlesh. All rights reserved.
+//
+
+import Foundation
+
+class InformationSummary{
+    
+    var title:String?
+    var description:String?
+    var imageRef:String?
+    
+    init(title:String?, description:String?, imageRef:String?){
+        
+        self.title = title
+        self.description = description
+        self.imageRef = imageRef
+    }
+}
