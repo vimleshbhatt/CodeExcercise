4b12a29752b7f7a293a0195285b127fac7fca65f
Vimlesh Bhatt <vimlesh.bhatt@fgb.ae>
2018-02-12 01:11:18 +0400

    Implemented a method to fetch data from the server and parsed the data into appropriate business objects

--- CodeExcercise/CodeExcercise/ViewControllers/HomeViewController.swift
+++ CodeExcercise/CodeExcercise/ViewControllers/HomeViewController.swift
@@ -27,6 +27,16 @@ class HomeViewController: UIViewController {
         }
     }
     
+    // Will use the URLSessionDataTask to fetch the information from remote server.
+    var dataTask: URLSessionDataTask?
+    
+    // Session to make the API call,
+    lazy var dataSession: URLSession = {
+        let configuration = URLSessionConfiguration.default
+        let session = URLSession(configuration: configuration, delegate: self as? URLSessionDelegate, delegateQueue: nil)
+        return session
+    }()
+    
     // MARK: ViewController Setup
     override public func viewDidLoad() {
         super.viewDidLoad()
@@ -54,6 +64,8 @@ class HomeViewController: UIViewController {
         // Create a table to show details about the country.
         tableView = UITableView(frame: self.view.frame, style: .grouped)
         tableView.backgroundColor = .clear
+        tableView.delegate = self
+        tableView.dataSource = self
         self.view.addSubview(tableView!)
         
         // Add constraints to the table and align it to the center of the view.
@@ -68,6 +80,7 @@ class HomeViewController: UIViewController {
         tableView.dg_addPullToRefreshWithActionHandler({ [weak self] () -> Void in
             DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(Int64(1.5 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: {
                 self?.tableView.dg_stopLoading()
+                self?.fetchDetails()
             })
             }, loadingView: loadingView)
         tableView.dg_setPullToRefreshFillColor(pullToRefreshFillBgColor)
@@ -80,6 +93,9 @@ class HomeViewController: UIViewController {
     fileprivate func registercells(){
         tableView.register(QuickInfoCell.self, forCellReuseIdentifier: "infoCell")
         tableView.estimatedRowHeight = 100
+        
+        // Initiate the call to fetch details.
+        fetchDetails()
     }
 }
 
@@ -116,6 +132,76 @@ extension HomeViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension HomeViewController{
+    
+    func fetchDetails(){
+        // Cancel is initiated for the task.
+        if dataTask != nil {
+            dataTask?.cancel()
+        }
+        // Start the network activity indicator to show the progress.
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        
+        // Prepare the url from where we will download the information
+        let url = URL(string: "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json")
+        
+        // Initiate the task with the url.
+        dataTask = dataSession.dataTask(with: url!) { data, response, error in
+            // Check for error and update the results to the table.
+            DispatchQueue.main.async {
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            }
+            
+            // Vheck for error and print the localized description for the same.
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let httpResponse = response as? HTTPURLResponse {
+                if httpResponse.statusCode == 200 {
+                    self.updateSearchResults(data)
+                }
+            }
+        }
+        
+        // Start the task
+        dataTask?.resume()
+    }
+    
+    // MARK: Handling Results
+    
+    // This helper method helps parse response JSON NSData into an array of InformationSummary objects.
+    func updateSearchResults(_ data: Data?) {
+        results.removeAll()
+        do {
+            
+            if let data = data, let response = try JSONSerialization.jsonObject(with: data, options:JSONSerialization.ReadingOptions.mutableContainers) as? [String: AnyObject] {
+                
+                // Get the results array
+                if let array: AnyObject = response["rows"] {
+                    
+                    for infoDictionary in array as! [AnyObject]{
+                        // Parse the search result into appropriate InformationSummary business model.
+                        let title = infoDictionary["title"] as? String
+                        let description = infoDictionary["description"] as? String
+                        let imageRef = infoDictionary["imageHref"] as? String
+                        results.append(InformationSummary(title: title, description: description, imageRef: imageRef))
+                    }
+                } else {
+                    print("Key not found in dictionary")
+                }
+            } else {
+                print("JSON Error")
+            }
+        } catch let error as NSError {
+            print("Error parsing results: \(error.localizedDescription)")
+        }
+        
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+            self.tableView.setContentOffset(CGPoint.zero, animated: false)
+        }
     }
 }
 
