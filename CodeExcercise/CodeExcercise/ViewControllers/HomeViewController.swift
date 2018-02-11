be920e6c290fa13107fbd0ab56dc2f95a3651462
Vimlesh Bhatt <vimlesh.bhatt@fgb.ae>
2018-02-11 23:59:07 +0400

    Created a table and added to the HomeViewController. Attach pull to refresh control to the table.

--- CodeExcercise/CodeExcercise/ViewControllers/HomeViewController.swift
+++ CodeExcercise/CodeExcercise/ViewControllers/HomeViewController.swift
@@ -8,12 +8,22 @@
 
 import Foundation
 import UIKit
+import DGElasticPullToRefresh
+
+let pullToRefreshFillBgColor = UIColor(red: 247/255.0, green: 180/255.0, blue: 68/255.0, alpha: 1.0)
+let pullToRefreshLoadingIndicatorTintColor = UIColor(red: 78/255.0, green: 221/255.0, blue: 200/255.0, alpha: 1.0)
 
 class HomeViewController: UIViewController {
+    
+    // MARK: Properties
+    var tableView: UITableView!
     
     // MARK: ViewController Setup
     override public func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Setup components in the view.
+        configureView()
     }
     
     override public func viewDidAppear(_ animated: Bool) {
@@ -21,7 +31,30 @@ class HomeViewController: UIViewController {
     }
     
     deinit {
+        tableView.dg_removePullToRefresh()
+    }
+    
+    // MARK: View Setup
+    fileprivate func configureView(){
         
+        // Setting background color of the navigationBar
+        UINavigationBar.appearance().barTintColor = pullToRefreshFillBgColor
+        
+        // Create a table to show details about the country.
+        tableView = UITableView(frame: self.view.frame, style: .grouped)
+        tableView.backgroundColor = .clear
+        self.view.addSubview(tableView!)
+        
+        // Add the Pull to refresh control.
+        let loadingView = DGElasticPullToRefreshLoadingViewCircle()
+        loadingView.tintColor = pullToRefreshLoadingIndicatorTintColor
+        tableView.dg_addPullToRefreshWithActionHandler({ [weak self] () -> Void in
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(Int64(1.5 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: {
+                self?.tableView.dg_stopLoading()
+            })
+            }, loadingView: loadingView)
+        tableView.dg_setPullToRefreshFillColor(pullToRefreshFillBgColor)
+        tableView.dg_setPullToRefreshBackgroundColor(tableView.backgroundColor!)
     }
 }
 
