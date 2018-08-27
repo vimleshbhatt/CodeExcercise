bf0b25a73a4b3557166b33ef6eaa9086858833a1
vimleshbhatt <vimlesh.bhatt@cognizant.com>
2018-08-27 15:55:53 +0400

    Test 6

--- CodeExcercise/CodeExcercise/AppDelegate.swift
+++ CodeExcercise/CodeExcercise/AppDelegate.swift
@@ -32,7 +32,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Make the controller visible on the window.
         window!.makeKeyAndVisible()
         
-        // Testing a new push 5
+        // Testing a new push 6
         
         return true
     }
